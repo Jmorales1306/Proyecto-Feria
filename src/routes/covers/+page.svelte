082cc682d2165dba76d06e81993897f3183f4549
Juan Morales <juancarlosmoralestellez@gmail.com>
2023-10-14 04:19:48 -0600

    Agrega parte parte de home

---
+++ src/routes/covers/+page.svelte
@@ -0,0 +1,5 @@
+<div class="w-[1920px] h-[451px] relative">
+    <div class="w-[1920px] h-[451px] left-0 top-0 absolute bg-gradient-to-r from-cyan-600 to-cyan-600"></div>
+    <div class="w-[405px] h-[121.83px] left-[98px] top-[103.09px] absolute text-white text-[64px] font-bold font-['Inter']">Covers</div>
+    <div class="w-[895px] h-[137px] left-[99px] top-[251px] absolute text-white text-[32px] font-normal font-['Inter']">Los covers más estilizados para darle identidad a tu smartphone.</div>
+</div>
