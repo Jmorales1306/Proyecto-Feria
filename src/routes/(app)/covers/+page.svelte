496e414a4d23b30c1c0ca34a9373c566522cb3b1
kecin1299 <kecin1299@gmail.com>
2023-10-18 23:37:23 -0600

    productos final

--- src/routes/(app)/covers/+page.svelte
+++ src/routes/(app)/covers/+page.svelte
@@ -26,7 +26,7 @@
 
   
 
-<div class="grid grid-rows-3 grid-flow-col gap-x-20 gap-y-40 m-20">
+<div class="grid md:grid-rows-3 sm:grid-rows-4 grid-flow-col gap-x-20 gap-y-40 m-20">
 
     <div class="flex justify-center items-center h-max bg-white rounded-xl">
         <div class="flex flex-col p-6 space-y-10 bg-white rounded-2xl shadow-2xl md:space-y-0 md:space-x1-10 md:m-0 md:p-8">
@@ -268,6 +268,116 @@
         </div>
     </div>
 </div>
+
+<div class="grid grid-col-4 grid-flow-col gap-20 justify-center mb-24">
+   
+    <button class="w-[100px] bg-white text-white text-xl font-bold rounded-xl p-2 mt-5">
+        <div class="px-8 py-4 pl-5 bg-[#2C3E50] rounded-3xl duration-200 group-hover:bg-sky-800 text-center">Ant.</div></button>
+
+    <button class="w-[10px] bg-white text-white text-xl font-bold rounded-xl p-2 mt-5">
+        <div class="px-8 py-4 bg-[#2C3E50] rounded-3xl duration-200 group-hover:bg-sky-800 text-center">1</div></button>
+        
+        <button class="w-[10px] bg-white text-gray-900 text-xl font-bold rounded-xl p-2 mt-5">
+            <div class="px-8 py-4 bg-slate-100 rounded-3xl duration-200 group-hover:bg-sky-800">2</div></button>
+ 
+        <button class="w-[10px] bg-white text-gray-900 text-xl font-bold rounded-xl p-2 mt-5">
+            <div class="px-8 py-4 bg-slate-100 rounded-3xl duration-200 group-hover:bg-sky-800">3</div></button>
+ 
+        <button class="w-[10px] bg-white text-gray-900 text-xl font-bold rounded-xl p-2 mt-5">
+            <div class="px-8 py-4 bg-slate-100 rounded-3xl duration-200 group-hover:bg-sky-800">4</div></button>
+
+            <button class="w-[100px] bg-white text-white text-xl font-bold rounded-xl p-2 mt-5">
+                <div class="px-8 py-4 pl-5 bg-[#2C3E50] rounded-3xl duration-200 group-hover:bg-sky-800 text-center">Sig.</div></button>        
+</div>
+
+<div>
+    <p class="justify-center text-center font-bold text-4xl">También podría interesarte.</p>
+</div>
+
+
+<div class="grid md:grid-col-3 sm:grid-col-4 grid-flow-col gap-x-20 gap-y-40 m-20 justify-center">
+
+    
+    <div class="flex justify-center items-center h-max bg-white rounded-xl">
+        <div class="flex flex-col p-6 space-y-10 bg-white rounded-2xl shadow-2xl md:space-y-0 md:space-x1-10 md:m-0 md:p-8">
+            <div>
+                <div class="bg-white rounded-lg mb-2">
+                    <img class="object-contain h-96 w-96" src={cover2} alt=""> 
+                </div>
+            </div>
+            <div class="flex flex-col space-y-4">
+                <div class="flex flex-col mb-4 space-y3 text-center md:text-left">
+                    <div class="flex flex-col mb-t space-y-3 text-center md:text-left">
+                        <p class="text-2xl font-bold text-center mt-5">$20</p>
+                    </div>
+                    <div class="max-w-sw text-2xl font-bold text-center mt-5">
+                        Cover Mi 11 Lite
+                    </div>
+                    
+                    
+                    <div class="group">
+                        <button class="w-full bg-white text-black text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-gray-100 rounded-lg duration-200 group-hover:bg-sky-200">Ver más</div></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="flex justify-center items-center h-min bg-white rounded-xl">
+        <div class="flex flex-col p-6 space-y-10 bg-white rounded-2xl shadow-2xl md:space-y-0 md:space-x1-10 md:m-0 md:p-8">
+            <div>
+                <div class="bg-white rounded-lg mb-2 ">
+                    <img class="object-contain h-96 w-96" src={cover3} alt=""> 
+                </div>
+            </div>
+            <div class="flex flex-col space-y-4">
+                <div class="flex flex-col mb-4 space-y3 text-center md:text-left">
+                    <div class="flex flex-col mb-t space-y-3 text-center md:text-left">
+                        <p class="text-2xl font-bold text-center mt-5">$20</p>
+                    </div>
+                    <div class="max-w-sw text-2xl font-bold text-center mt-5">
+                        Cover Redmi Note 
+                    </div>
+                    
+                    
+                    <div class="group">
+                        <button class="w-full bg-white text-black text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-gray-100 rounded-lg duration-200 group-hover:bg-sky-200">Ver más</div></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="flex justify-center items-center h-min bg-white rounded-xl">
+        <div class="flex flex-col p-6 space-y-10 bg-white rounded-2xl shadow-2xl md:space-y-0 md:space-x1-10 md:m-0 md:p-8">
+            <div>
+                <div class="bg-white rounded-lg mb-2 ">
+                    <img class="object-contain h-96 w-96" src={cover4} alt=""> 
+                </div>
+            </div>
+            <div class="flex flex-col space-y-4">
+                <div class="flex flex-col mb-4 space-y3 text-center md:text-left">
+                    <div class="flex flex-col mb-t space-y-3 text-center md:text-left">
+                        <p class="text-2xl font-bold text-center mt-5">$20</p>
+                    </div>
+                    <div class="max-w-sw text-2xl font-bold text-center mt-5">
+                        Cover Realme C21
+                    </div>
+                    
+                    
+                    <div class="group">
+                        <button class="w-full bg-white text-black text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-gray-100 rounded-lg duration-200 group-hover:bg-sky-200">Ver más</div></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 
 
 
