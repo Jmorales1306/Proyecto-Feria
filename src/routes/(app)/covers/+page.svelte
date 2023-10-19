d0455aaf51a8b9077651d1f4962b8f312e0229bc
kecin1299 <kecin1299@gmail.com>
2023-10-18 20:02:42 -0600

    warranty, imágenes, colores, productos

--- src/routes/(app)/covers/+page.svelte
+++ src/routes/(app)/covers/+page.svelte
@@ -1,5 +1,303 @@
+<script>
+    import facebook from "$lib/assets/facebook-circle-logo-48.png" 
+    import twitter from "$lib/assets/twitter-logo-48.png"
+    import instagram from "$lib/assets/instagram-alt-logo-48.png"
+    import headphone from "$lib/assets/audifonos.png"
+</script>
+
+
 <div class="w-[1920px] h-[451px] relative">
-    <div class="w-[1920px] h-[451px] left-0 top-0 absolute bg-gradient-to-r from-cyan-600 to-cyan-600"></div>
+    <div class="xl:flex max-w-[300px] pr-20 pt-10 end">
+        <img src={headphone} alt="">
+  
+    <div class="w-[1920px] h-[451px] left-0 top-0 absolute bg-gradient-to-r from-sky-600 to-cyan-100"></div>
     <div class="w-[405px] h-[121.83px] left-[98px] top-[103.09px] absolute text-white text-[64px] font-bold font-['Inter']">Covers</div>
     <div class="w-[895px] h-[137px] left-[99px] top-[251px] absolute text-white text-[32px] font-normal font-['Inter']">Los covers más estilizados para darle identidad a tu smartphone.</div>
+    
+    
+    </div>
+  </div>
+
+  
+
+<div class="grid grid-rows-3 grid-flow-col gap-x-20 gap-y-40 m-20">
+
+    <div class="flex justify-center items-center h-max bg-slate-100 rounded-xl">
+        <div class="flex flex-col p-6 space-y-10 bg-white rounded-2xl shadow-2xl md:space-y-0 md:space-x1-10 md:m-0 md:p-8">
+            <div>
+                <div class="bg-white rounded-lg mb-2">
+                    <img src={headphone} alt=""> 
+                </div>
+            </div>
+            <div class="flex flex-col space-y-4">
+                <div class="flex flex-col mb-4 space-y3 text-center md:text-left">
+                    <div class="flex flex-col mb-t space-y-3 text-center md:text-left">
+                        <p class="text-2xl font-bold text-center mt-5">$20</p>
+                    </div>
+                    <div class="max-w-sw text-2xl font-bold text-center mt-5">
+                        Audífonos Sony 
+                    </div>
+                    
+                    <div class="group">
+                        <button class="w-full bg-white text-white text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-[#2C3E50] rounded-lg duration-200 group-hover:bg-sky-800">Comprar</div></button>
+                            
+                    </div>
+                    <div class="group">
+                        <button class="w-full bg-white text-black text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-gray-100 rounded-lg duration-200 group-hover:bg-sky-200">Ver más</div></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="flex justify-center items-center h-min bg-slate-100 rounded-xl">
+        <div class="flex flex-col p-6 space-y-10 bg-white rounded-2xl shadow-2xl md:space-y-0 md:space-x1-10 md:m-0 md:p-8">
+            <div>
+                <div class="bg-white rounded-lg mb-2">
+                    <img src={headphone} alt=""> 
+                </div>
+            </div>
+            <div class="flex flex-col space-y-4">
+                <div class="flex flex-col mb-4 space-y3 text-center md:text-left">
+                    <div class="flex flex-col mb-t space-y-3 text-center md:text-left">
+                        <p class="text-2xl font-bold text-center mt-5">$20</p>
+                    </div>
+                    <div class="max-w-sw text-2xl font-bold text-center mt-5">
+                        Audífonos Sony 
+                    </div>
+                    
+                    <div class="group">
+                        <button class="w-full bg-white text-white text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-[#2C3E50] rounded-lg duration-200 group-hover:bg-sky-800">Comprar</div></button>
+                            
+                    </div>
+                    <div class="group">
+                        <button class="w-full bg-white text-black text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-gray-100 rounded-lg duration-200 group-hover:bg-sky-200">Ver más</div></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-center items-center h-min bg-slate-100 rounded-xl">
+        <div class="flex flex-col p-6 space-y-10 bg-white rounded-2xl shadow-2xl md:space-y-0 md:space-x1-10 md:m-0 md:p-8">
+            <div>
+                <div class="bg-white rounded-lg mb-2">
+                    <img src={headphone} alt=""> 
+                </div>
+            </div>
+            <div class="flex flex-col space-y-4">
+                <div class="flex flex-col mb-4 space-y3 text-center md:text-left">
+                    <div class="flex flex-col mb-t space-y-3 text-center md:text-left">
+                        <p class="text-2xl font-bold text-center mt-5">$20</p>
+                    </div>
+                    <div class="max-w-sw text-2xl font-bold text-center mt-5">
+                        Audífonos Sony 
+                    </div>
+                    
+                    <div class="group">
+                        <button class="w-full bg-white text-white text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-[#2C3E50] rounded-lg duration-200 group-hover:bg-sky-800">Comprar</div></button>
+                            
+                    </div>
+                    <div class="group">
+                        <button class="w-full bg-white text-black text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-gray-100 rounded-lg duration-200 group-hover:bg-sky-200">Ver más</div></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-center items-center h-min bg-slate-100 rounded-xl">
+        <div class="flex flex-col p-6 space-y-10 bg-white rounded-2xl shadow-2xl md:space-y-0 md:space-x1-10 md:m-0 md:p-8">
+            <div>
+                <div class="bg-white rounded-lg mb-2">
+                    <img src={headphone} alt=""> 
+                </div>
+            </div>
+            <div class="flex flex-col space-y-4">
+                <div class="flex flex-col mb-4 space-y3 text-center md:text-left">
+                    <div class="flex flex-col mb-t space-y-3 text-center md:text-left">
+                        <p class="text-2xl font-bold text-center mt-5">$20</p>
+                    </div>
+                    <div class="max-w-sw text-2xl font-bold text-center mt-5">
+                        Audífonos Sony 
+                    </div>
+                    
+                    <div class="group">
+                        <button class="w-full bg-white text-white text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-[#2C3E50] rounded-lg duration-200 group-hover:bg-sky-800">Comprar</div></button>
+                            
+                    </div>
+                    <div class="group">
+                        <button class="w-full bg-white text-black text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-gray-100 rounded-lg duration-200 group-hover:bg-sky-200">Ver más</div></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-center items-center h-min bg-slate-100 rounded-xl">
+        <div class="flex flex-col p-6 space-y-10 bg-white rounded-2xl shadow-2xl md:space-y-0 md:space-x1-10 md:m-0 md:p-8">
+            <div>
+                <div class="bg-white rounded-lg mb-2">
+                    <img src={headphone} alt=""> 
+                </div>
+            </div>
+            <div class="flex flex-col space-y-4">
+                <div class="flex flex-col mb-4 space-y3 text-center md:text-left">
+                    <div class="flex flex-col mb-t space-y-3 text-center md:text-left">
+                        <p class="text-2xl font-bold text-center mt-5">$20</p>
+                    </div>
+                    <div class="max-w-sw text-2xl font-bold text-center mt-5">
+                        Audífonos Sony 
+                    </div>
+                    
+                    <div class="group">
+                        <button class="w-full bg-white text-white text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-[#2C3E50] rounded-lg duration-200 group-hover:bg-sky-800">Comprar</div></button>
+                            
+                    </div>
+                    <div class="group">
+                        <button class="w-full bg-white text-black text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-gray-100 rounded-lg duration-200 group-hover:bg-sky-200">Ver más</div></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-center items-center h-min bg-slate-100 rounded-xl">
+        <div class="flex flex-col p-6 space-y-10 bg-white rounded-2xl shadow-2xl md:space-y-0 md:space-x1-10 md:m-0 md:p-8">
+            <div>
+                <div class="bg-white rounded-lg mb-2">
+                    <img src={headphone} alt=""> 
+                </div>
+            </div>
+            <div class="flex flex-col space-y-4">
+                <div class="flex flex-col mb-4 space-y3 text-center md:text-left">
+                    <div class="flex flex-col mb-t space-y-3 text-center md:text-left">
+                        <p class="text-2xl font-bold text-center mt-5">$20</p>
+                    </div>
+                    <div class="max-w-sw text-2xl font-bold text-center mt-5">
+                        Audífonos Sony 
+                    </div>
+                    
+                    <div class="group">
+                        <button class="w-full bg-white text-white text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-[#2C3E50] rounded-lg duration-200 group-hover:bg-sky-800">Comprar</div></button>
+                            
+                    </div>
+                    <div class="group">
+                        <button class="w-full bg-white text-black text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-gray-100 rounded-lg duration-200 group-hover:bg-sky-200">Ver más</div></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-center items-center h-min bg-slate-100 rounded-xl">
+        <div class="flex flex-col p-6 space-y-10 bg-white rounded-2xl shadow-2xl md:space-y-0 md:space-x1-10 md:m-0 md:p-8">
+            <div>
+                <div class="bg-white rounded-lg mb-2">
+                    <img src={headphone} alt=""> 
+                </div>
+            </div>
+            <div class="flex flex-col space-y-4">
+                <div class="flex flex-col mb-4 space-y3 text-center md:text-left">
+                    <div class="flex flex-col mb-t space-y-3 text-center md:text-left">
+                        <p class="text-2xl font-bold text-center mt-5">$20</p>
+                    </div>
+                    <div class="max-w-sw text-2xl font-bold text-center mt-5">
+                        Audífonos Sony 
+                    </div>
+                    
+                    <div class="group">
+                        <button class="w-full bg-white text-white text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-[#2C3E50] rounded-lg duration-200 group-hover:bg-sky-800">Comprar</div></button>
+                            
+                    </div>
+                    <div class="group">
+                        <button class="w-full bg-white text-black text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-gray-100 rounded-lg duration-200 group-hover:bg-sky-200">Ver más</div></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-center items-center h-min bg-slate-100 rounded-xl">
+        <div class="flex flex-col p-6 space-y-10 bg-white rounded-2xl shadow-2xl md:space-y-0 md:space-x1-10 md:m-0 md:p-8">
+            <div>
+                <div class="bg-white rounded-lg mb-2">
+                    <img src={headphone} alt=""> 
+                </div>
+            </div>
+            <div class="flex flex-col space-y-4">
+                <div class="flex flex-col mb-4 space-y3 text-center md:text-left">
+                    <div class="flex flex-col mb-t space-y-3 text-center md:text-left">
+                        <p class="text-2xl font-bold text-center mt-5">$20</p>
+                    </div>
+                    <div class="max-w-sw text-2xl font-bold text-center mt-5">
+                        Audífonos Sony 
+                    </div>
+                    
+                    <div class="group">
+                        <button class="w-full bg-white text-white text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-[#2C3E50] rounded-lg duration-200 group-hover:bg-sky-800">Comprar</div></button>
+                            
+                    </div>
+                    <div class="group">
+                        <button class="w-full bg-white text-black text-2xl font-bold rounded-xl p-2 mt-5">
+                            <div class="px-8 py-4 bg-gray-100 rounded-lg duration-200 group-hover:bg-sky-200">Ver más</div></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+
+
+
+<footer class="bg-[#2C465D] py-12">
+    <div class="container mx-auto px-2 text-white">
+        <div class="flex justify-start">
+            <div class="pr-10">
+                <h2 class="text-3xl font-bold mb-4">Soporte</h2>
+                <ul>
+                    <li><a href="#">Garantía de productos</a></li>
+                    <li><a href="#">Guías de uso</a></li>
+                    <li><a href="#">Ubicación</a></li>
+                </ul>
+            </div>
+            <div class="pr-10">
+                <h2 class="text-3xl font-bold mb-4">Sobre nosotros</h2>
+                <ul>
+                    <li><a href="#">H Ventas Locas</a></li>
+                    <li><a href="#">Políticas de privacidad</a></li>
+                    <li><a href="#">Términos y condiciones</a></li>
+                </ul>
+            </div>
+            <div class="pr-10">
+                <h2 class="text-3xl font-bold mb-4">Contáctanos</h2>
+                <ul>
+                    <li><a href="#">Escribe a nuestro WhatsApp</a></li>
+                    
+                </ul>
+            </div>
+            <div>
+                <h2 class="text-3xl font-bold mb-4">Síguenos</h2>
+                <ul class="flex space-x-2 text-6xl">
+                    <li><img src={facebook} alt=""></li>
+                    <li><img src={twitter} alt=""></li>
+                    <li><img src={instagram} alt=""></li>
+                    
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="grid justify-items-center text-white mt-20">
+        <p>© 2023 H ventas Locas. Todos los derechos reservados.</p>
+    </div>
+</footer>
---
+++ src/routes/(app)/warranty/+page.svelte
@@ -0,0 +1,135 @@
+<script>
+    import headphone from '$lib/assets/headphone-regular-240.png'
+    import icon from '$lib/assets/check-circle-solid-240 (1).png'
+    import phone from '$lib/assets/game-solid-240.png'
+    import facebook from "$lib/assets/facebook-circle-logo-48.png" 
+    import twitter from "$lib/assets/twitter-logo-48.png"
+    import instagram from "$lib/assets/instagram-alt-logo-48.png"
+  </script>
+
+<body class="bg:[#F8F8F8] ">
+    <section class="services mx-8 mt-16 mb">
+        <div class="bg-gradient-to-r from-sky-100 to-indigo-100 max-w[1466px] mx-4 xl:mx-auto 
+        rounded-[20px] xl:pt-12 px-6 xl:px-5 relative h-[368px] flex items-center
+        xl:items-start">
+        <div class="container pl-5 pr-5 mx-auto">
+            <div class="hero__text xl:w-[48%] text-center xl:text-left">
+                <h2 class=" mb-5 font-bold px-5 items-center text-2xl">
+                    Política de garantía
+    
+                </h2>
+                <p class="px-5 mb-[5px] text-justify text-lg">
+                    En H Ventas Locas, aseguramos que al comprar un producto, está obteniendo calidad y confiabilidad con el mismo, en caso de presentar fallos, puede reclamarlo en un lapso de 72 horas para el caso de los accesorios, y de 60 días en smartphones, para esto debe cumplir con ciertas condiciones que debe tomar en cuenta si desea realizar un reclamo.
+                </p>
+                
+            </div>
+    
+    
+        </div>
+        <div class="hero__img hidden xl:flex max-w-[300px] pr-20 pt-10 center ">
+            <img src={icon} alt="">
+        </div>
+    </div>
+    </section>
+    
+    <section class="services mx-8 mt-16 mb">
+        <div class="bg-gradient-to-r from-cyan-100 to-blue-100 max-w[1466px] mx-4 xl:mx-auto 
+        rounded-[20px] xl:pt-12 px-6 xl:px-5 relative h-[368px] flex items-center
+        xl:items-start">
+        <div class="container pl-5 pr-5 mx-auto">
+            <div class="hero__text xl:w-[48%] text-center xl:text-left">
+                <h2 class=" mb-5 font-bold px-5 items-center text-2xl">
+                    Condiciones de garantía de accesorios
+    
+                </h2>
+                
+                <ol class="px-5 mb-[10px] text-justify text-lg">
+                    <li class="mb-[10px] ">
+                        El cliente puede realizar un reclamo en un lapso de 72 horas después de comprado el producto.
+                    </li>
+                    <li class="mb-[10px] ">El producto debe presentar un fallo de fábrica, no generado por el uso indebido por parte del cliente.</li>
+                    <li class="mb-[10px] ">En caso de cumplir con las condiciones, tiene derecho a un cambio de producto o reembolso.</li>
+                    <li class="mb-[10px] ">El cambio de producto también puede ser por otro de igual o similar valor.</li>
+                <ol>
+            </div>
+    
+    
+        </div>
+        <div class="hero__img hidden xl:flex max-w-[300px] pr-20 pt-10 center ">
+            <img src={headphone} alt="">
+        </div>
+    </div>
+    </section>
+    
+    
+    <section class="services mx-8 mt-16 mb-16">
+        <div class="bg-gradient-to-r from-sky-100 to-indigo-100 max-w[1466px] mx-4 xl:mx-auto 
+        rounded-[20px] xl:pt-12 px-6 xl:px-5 relative h-[368px] flex items-center
+        xl:items-start">
+        <div class="container pl-5 pr-5 mx-auto">
+            <div class="hero__text xl:w-[48%] text-center xl:text-left">
+                <h2 class=" mb-5 font-bold px-5 items-center text-2xl">
+                    Condiciones de garantía de SmartPhones
+    
+                </h2>
+    
+                <ol class="px-5 mb-[10px] text-justify items-center text-lg">
+                    
+                    <li class="mb-[10px] ">El smartphone debe presentar un daño de fábrica y no ocasionado por el cliente.</li>
+                    <li class="mb-[10px] ">En caso de cumplir las condiciones, puede solicitar un cambio de producto o un reembolso.</li>
+                    <li class="mb-[100px] ">El cambio del producto también puede ser por otro de igual o similar valor.</li>
+                <ol>
+                
+                
+            </div>
+    
+    
+        </div>
+        <div class="hero__img hidden xl:flex max-w-[300px] pr-20 pt-10 center ">
+            <img src={phone} alt="">
+        </div>
+    </div>
+    </section>  
+</body>
+
+<footer class="bg-[#2C465D] py-12">
+    <div class="container mx-auto px-2 text-white">
+        <div class="flex justify-start">
+            <div class="pr-10">
+                <h2 class="text-3xl font-bold mb-4">Soporte</h2>
+                <ul>
+                    <li><a href="#">Garantía de productos</a></li>
+                    <li><a href="#">Guías de uso</a></li>
+                    <li><a href="#">Ubicación</a></li>
+                </ul>
+            </div>
+            <div class="pr-10">
+                <h2 class="text-3xl font-bold mb-4">Sobre nosotros</h2>
+                <ul>
+                    <li><a href="#">H Ventas Locas</a></li>
+                    <li><a href="#">Políticas de privacidad</a></li>
+                    <li><a href="#">Términos y condiciones</a></li>
+                </ul>
+            </div>
+            <div class="pr-10">
+                <h2 class="text-3xl font-bold mb-4">Contáctanos</h2>
+                <ul>
+                    <li><a href="#">Escribe a nuestro WhatsApp</a></li>
+                    
+                </ul>
+            </div>
+            <div>
+                <h2 class="text-3xl font-bold mb-4 mr-7">Síguenos</h2>
+                <ul class="flex space-x-2 text-6xl">
+                    <li><img src={facebook} alt=""></li>
+                    <li><img src={twitter} alt=""></li>
+                    <li><img src={instagram} alt=""></li>
+                    
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="grid justify-items-center text-white mt-20">
+        <p>© 2023 H ventas Locas. Todos los derechos reservados.</p>
+    </div>
+</footer>
