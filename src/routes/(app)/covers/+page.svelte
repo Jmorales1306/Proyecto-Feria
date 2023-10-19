d150302d70ed7fe2587fd01fa75d2bf67d53494d
Juan Morales <juancarlosmoralestellez@gmail.com>
2023-10-19 05:48:06 -0600

    Agrega Home

--- src/routes/(app)/covers/+page.svelte
+++ src/routes/(app)/covers/+page.svelte
@@ -8,13 +8,10 @@
 
 <div class="w-[1920px] h-[451px] relative">
     <div class="xl:flex max-w-[300px] pr-20 pt-10 end">
-        <img src={headphone} alt="">
-  
+    <img src={headphone} alt="">
     <div class="w-[1920px] h-[451px] left-0 top-0 absolute bg-gradient-to-r from-sky-600 to-cyan-100"></div>
     <div class="w-[405px] h-[121.83px] left-[98px] top-[103.09px] absolute text-white text-[64px] font-bold font-['Inter']">Covers</div>
     <div class="w-[895px] h-[137px] left-[99px] top-[251px] absolute text-white text-[32px] font-normal font-['Inter']">Los covers más estilizados para darle identidad a tu smartphone.</div>
-    
-    
     </div>
   </div>
 
